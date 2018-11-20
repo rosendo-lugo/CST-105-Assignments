baf8aa34b4eaffabeac99ab9545aef126c68d3ac
rolugo <rosendo.lugo@gmail.com>
2018-11-20 01:16:52 -0700

    no message

--- Final Project/Milestone 2/NFLPlayer.java
+++ Final Project/Milestone 2/NFLPlayer.java
@@ -1,27 +1,34 @@
+
+
 public class NFLPlayer {
 
 	public String name;
-	 int age;
-	 double weight;
-	 double height;
-	 int completions;
-	 int passingAttempts;
-     double passingPercent;
-     double passingYards;
-     double passingAverage;
-     int passingTouchDowns;	
-   	 int interception;
-   	 int sacks;
-	 double rating;   	 
-	 int rushingAttempts;
-	 double rushingYards;
-	 double rushingAverage;
-	 int rushingTouchDowns;
-	 int fumbles;
+	public int age;
+	public double weight;
+	public double height;
+	public int completions;
+	public int passingAttempts;
+	public double passingPercent;
+	public int passingYards;
+	public double passingAverage;
+	public int passingTouchDowns;	
+	public int interception;
+	public int sacks;
+	public double rating; 
+	public int receivingReception;
+	public int receivingYards;
+	public double receivingAverage;
+	public int receivingTouchDown;
+	public int rushingAttempts;
+	public int rushingYards;
+	public double rushingAverage;
+	public int rushingTouchDowns;
+	public int fumbles;
 	public NFLPlayer() {
 	}
-	public NFLPlayer(String name, int age, double weight, double height, int completions, int passingAttempts, double passingPercent, double passingYards, double passingAverage, int passingTouchDowns,
-			int interception, int sacks, double rating, int rushingAttempts, double rushingYards, double rushingAverage, int rushingTouchDowns, int fumbles) {
+	public NFLPlayer(String name, int age, double weight, double height, int completions, int passingAttempts, double passingPercent, int passingYards, 
+			double passingAverage, int passingTouchDowns, int interception, int sacks, double rating, int receivingReception, int receivingYards, 
+			double receivingAverage, int receivingTouchDown, int rushingAttempts, int rushingYards, double rushingAverage, int rushingTouchDowns, int fumbles) {
 
 		this.name = name;
 		this.age = age;
@@ -35,7 +42,11 @@ public class NFLPlayer {
 		this.passingTouchDowns = passingTouchDowns;
 		this.interception = interception;
 		this.sacks = sacks;
-		this.rating = rating;		
+		this.rating = rating;
+		this.receivingReception = receivingReception;
+		this.receivingYards = receivingYards;
+		this.receivingAverage = receivingAverage;
+		this.receivingTouchDown = receivingTouchDown;
 		this.rushingAttempts = rushingAttempts;
 		this.rushingYards = rushingYards;
 		this.rushingAverage = rushingAverage;
@@ -92,11 +103,11 @@ public class NFLPlayer {
 	public double getPassingPercent() {
 		return passingPercent;
 	}
-	public void setPassingPercent(int passingPercent) {
+	public void setPassingPercent(double passingPercent) {
 		this.passingPercent = passingPercent;
 		
 	}
-	public double getPassingYards() {
+	public int getPassingYards() {
 		return passingYards;
 	}
 	public void setPassingYards(int passingYards) {
@@ -106,7 +117,7 @@ public class NFLPlayer {
 	public double getPassingAverage() {
 		return passingAverage;
 	}
-	public void setPassingAverage(int passingAverage) {
+	public void setPassingAverage(double passingAverage) {
 		this.passingAverage = passingAverage;
 		
 	}
@@ -117,17 +128,17 @@ public class NFLPlayer {
 		this.passingTouchDowns = passingTouchDowns;
 
 	}
-	public int getinterception() {
+	public int getInterception() {
 		return interception;
 	}
-	public void setinterception(int interception) {
+	public void setInterception(int interception) {
 		this.interception = interception;
 
 	}
-	public int getsacks() {
+	public int getSacks() {
 		return sacks;
 	}
-	public void setsacks(int sacks) {
+	public void setSacks(int sacks) {
 		this.sacks = sacks;
 
 	}
@@ -136,6 +147,35 @@ public class NFLPlayer {
 	}
 	public void setRating(double rating) {
 		this.rating = rating;
+		
+	}
+	public int getReceivingReception() {
+		return receivingReception;
+	}
+	public void setReceivingRecepetion(int receivingReception) {
+		this.receivingReception = receivingReception;
+		
+	}
+	public int getReceivingYards() {
+		return receivingYards;
+	}
+	public void setReceivingYards(int receivingYards) {
+		this.receivingYards = receivingYards;
+		
+	}
+	public double getReceivingAverage() {
+		return receivingAverage;
+	}
+	public void setReceivingAverage(double receivingAverage) {
+		this.receivingAverage = receivingAverage;
+		
+	}
+	public int getReceivingTouchDown() {
+		return receivingTouchDown;
+	}
+	public void setReceivingTouchDown(int receivingTouchDown) {
+		this.receivingTouchDown = receivingTouchDown;
+		
 	}
 	public int getRushingAttempts() {
 		return rushingAttempts;
@@ -144,22 +184,22 @@ public class NFLPlayer {
 		this.rushingAttempts = rushingAttempts;
 		
 	}
-	public double getRushingYards() {
+	public int getRushingYards() {
 		return rushingYards;
 	}
 	public void setRushingYards(int rushingYards) {
 		this.rushingYards = rushingYards;
-
-	}
-	public int getRushingTouchDowns() {
-		return rushingTouchDowns;
+		
 	}
 	public double getRushingAverage() {
 		return rushingAverage;
 	}
-	public void setRushingAverage(int rushingAverage) {
+	public void setRushingAverage(double rushingAverage) {
 		this.rushingAverage = rushingAverage;
-
+		
+	}
+	public int getRushingTouchDowns() {
+		return rushingTouchDowns;
 	}
 	public void setRushingTouchDowns(int rushingTouchDowns) {
 		this.rushingTouchDowns = rushingTouchDowns;
@@ -171,21 +211,21 @@ public class NFLPlayer {
 	public void setFumbles(int fumbles) {
 		this.fumbles = fumbles;
 
-
 	}
 	public static void main(String[] args) {
 
 
-		NFLPlayer prescott = new NFLPlayer("Dak Prescott", 25, 235, 74, 816, 1254, 65.1, 9129, 7.3, 56, 22, 91, 94.2, 166, 900, 5.4, 16, 21);
+		NFLPlayer romo = new NFLPlayer();
+		romo.setName("Tony Romo");
+		System.out.println("Testing no-arg constructor, setters, & toString method:\n" + romo.toString());
+
+		NFLPlayer prescott = new NFLPlayer("Dak Prescott", 25, 235, 74, 816, 1254, 65.1, 9129, 7.3, 56, 22, 91, 94.2, 0, 0, 0, 0, 166, 900, 5.4, 16, 21);
 		prescott.setAge(23);
 		prescott.setWeight(250);
 		System.out.println("\nTesting narg constructor, setters, and getters:\nDaK Prescott age is not " + prescott.getAge()
-				+ " years old nor is " + prescott.getWeight() + " lbs, but he's " 
-				+ prescott.getHeight() + " inches tall");
+				+ " years old nor he's " + prescott.getWeight() + " lbs, but he's " + prescott.getHeight() + " inches tall");
 		
-		NFLPlayer romo = new NFLPlayer();
-				romo.setName("Tony Romo");
-		System.out.println("Testing no-arg constructor, setters, & toString method:\n" + romo.toString());
+
 
 	}
 }
